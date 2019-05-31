6534eb0bd5f325e8866685b0dafdd5d3346e6539
MenacingDwarf <MenacingDwarf@yandex.ru>
2019-05-31 09:48:40 +0300

    Update ALLOWED_HOSTS with site and development server URL

--- OpenWay/settings.py
+++ OpenWay/settings.py
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+ALLOWED_HOSTS = ['openwayregister.herokuapp.com','127.0.0.1']
